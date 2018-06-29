c5e98227d4143d93b5074a3b9fbb9b0ae634117f
barcher <bennion.archer@gmail.com>
2018-06-28 18:30:27 -0600

    local Mutations! No more kendo grid!

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
-import { ApolloConsumer } from 'react-apollo'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 
 import { Button } from '@progress/kendo-react-buttons'
 
 export class Category extends Component {
   render() {
-    return <div className="Category">{this.props.Category.name}</div>
+    return (
+      <Mutation
+        mutation={TOGGLE_SELECTED}
+        variables={{ id: this.props.Category.id }}>
+        {toggleSelected => (
+          <Button
+            onClick={toggleSelected}
+            className="Category"
+            primary={this.props.Category.selected}>
+            {this.props.Category.name}
+          </Button>
+        )}
+      </Mutation>
+    )
   }
 }
+
+const TOGGLE_SELECTED = gql`
+  mutation toggleSelectedPFC($id: String!) {
+    setCategoryGridItemSelected(id: $id) @client
+  }
+`
 
 export default Category
--- src/Components/CategoryGrid.js
+++ src/Components/CategoryGrid.js
@@ -1,44 +1,11 @@
 import React, { Component } from 'react'
 import { graphql, Query } from 'react-apollo'
 import gql from 'graphql-tag'
+import Category from './Category'
 
 import { Grid, GridColumn as Column } from '@progress/kendo-react-grid'
 
 export class CategoryGrid extends Component {
-  state = {
-    lastSelectedIndex: 0,
-    data: []
-  }
-  selectionChange = event => {
-    event.dataItem = {
-      ...event.dataItem,
-      selected: !event.dataItem.selected
-    }
-    this.forceUpdate()
-  }
-
-  rowClick = event => {
-    let last = this.state.lastSelectedIndex
-    let current = this.data.findIndex(dataItem => dataItem === event.dataItem)
-
-    if (!event.nativeEvent.shiftKey) {
-      //TODO set last selected index
-      this.lastSelectedIndex = last = current
-    }
-
-    let select = !event.dataItem.selected
-    for (let i = Math.min(last, current); i <= Math.max(last, current); i++) {
-      this.data[i] = { ...this.data[i], selected: select }
-    }
-    this.forceUpdate()
-  }
-
-  headerSelectionChange = event => {
-    const checked = event.syntheticEvent.target.checked
-    //WHOOOOP
-    this.data.forEach(item => (item.selected = checked))
-    this.forceUpdate()
-  }
   render() {
     if (this.props.getState && this.props.getState.loading) {
       return <div>Loading</div>
@@ -51,26 +18,14 @@ export class CategoryGrid extends Component {
       return <div>Error</div>
     }
 
-    const state = this.props
-    this.data = Array.from(this.props.allProductFamilyCategories)
-    console.log('you mean THIS state!?  ' + JSON.stringify(this.data))
+    const state = this.props.categories
+    const categoriesToRender = this.props.allProductFamilyCategories
 
     return (
       <div className="CategoryGrid">
-        <Grid
-          data={this.data}
-          selectedField="selected"
-          headerSelectionValue={
-            this.data.findIndex(dataItem => dataItem.selected === false) === -1
-          }
-          onSelectionChange={this.selectionChange}
-          onHeaderSelectionChange={this.headerSelectionChange}
-          onRowClick={this.rowClick}>
-          <Column field="name" title="Category" />
-          <Column field="discipline" title="discipline" />
-          <Column field="firm.name" title="firm" />
-          <Column field="selected" />
-        </Grid>
+        {categoriesToRender.map(category => (
+          <Category key={category.id} Category={category} />
+        ))}
       </div>
     )
   }
